c37457b9f70a325d742225eeda284d74e6e0a0d8
Emaad_home <emaadnaiyar208@gmail.com>
2026-09-16 17:35:29 +0530

    Separate variable declarations in main.c
    Refactor variable declarations for clarity.

--- main.c
+++ main.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 #include "product.h"
 int main(){
-	double a,b,p;
+	double a,b;
+	double p;
 	printf("enter the numbers 1  and 2 :");
 	scanf("%lf %lf",&a , &b);
 	p = product(a,b);
